59ad2e75e5ae5f8c4ee8365fcb6a1e3fefcef5ff
Заур <zaurmag411@gmail.com>
2021-07-01 14:45:29 +0300

    + Добавил Axios
    + Добавил Axios

---
+++ src/axios/dbase.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+import store from '../store'
+import router from '../router'
+
+const dbtAxios = axios.create({
+  baseURL: process.env.VUE_APP_DB_URL
+})
+
+dbtAxios.defaults.params = {}
+
+dbtAxios.interceptors.request.use(async config => {
+  if (!store.getters['auth/isAuthenticated']) {
+    return config
+  }
+
+  if (store.getters['auth/isExpired']) {
+    await store.dispatch('auth/refresh')
+  }
+
+  config.params['auth'] = store.getters['auth/token']
+
+  return config
+})
+
+// Add a response interceptor
+dbtAxios.interceptors.response.use(null, error => {
+  if (error.response.status === 401) {
+    store.commit('auth/logout')
+    router.push('/auth?message=auth')
+  }
+
+  return Promise.reject(error)
+})
+
+export default dbtAxios
