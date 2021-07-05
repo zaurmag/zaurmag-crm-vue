fe0c937a14d9375e5aed1f8b1d20a58e556222f2
Заур <zaurmag411@gmail.com>
2021-07-05 22:53:38 +0300

    + Постраничная навигация

--- src/components/ui/AppPagination.vue
+++ src/components/ui/AppPagination.vue
@@ -1,28 +1,28 @@
 <template>
   <nav class="pagination" v-if="items > 1">
-    <div class="row align-items-center gy-3">
-      <div class="col-sm">
-        <div class="pagination__pagesize">
+    <div class="row align-items-center gy-3 justify-content-between">
+      <div class="col-sm-3">
+        <div class="pagination__pagesize align-items-center">
           <div class="text-secondary me-2">Показать:</div>
-          <select v-model="selectSize" @change="$emit('changeSize', selectSize)">
+          <select class="form-select form-select-sm" v-model="selectSize" @change="$emit('changeSize', selectSize)">
             <option value="4">4</option>
             <option value="8">8</option>
             <option value="12">12</option>
             <option value="16">16</option>
           </select>
           <div class="text-secondary ms-3">из</div>
-          <div class="text-secondary ms-2">20</div>
+          <div class="text-secondary ms-2">{{ count }}</div>
         </div>
       </div>
       <div class="col-sm-auto">
         <ul class="pagination__list">
-          <li class="pagination__nav pagination__nav--start" :class="{'is-hidden': modelValue === 1}">
+          <li class="pagination__item" :class="{'is-disabled': modelValue === 1}">
             <a class="pagination__link" href="#" @click.prevent="$emit('update:modelValue', modelValue - 1)">Предыдущая</a>
           </li>
-          <li class="pagination__item" v-for="p in items" :key="p">
-            <a href="#" @click.prevent="$emit('update:modelValue', p)" :class="['pagination__link', {'is-active': p === modelValue}]">{{ p }}</a>
+          <li class="pagination__item" :class="[{'is-active': p === modelValue}]" v-for="p in items" :key="p">
+            <a href="#" @click.prevent="$emit('update:modelValue', p)" class="pagination__link">{{ p }}</a>
           </li>
-          <li class="pagination__nav pagination__nav--end" :class="{'is-hidden': modelValue === items}">
+          <li class="pagination__item" :class="{'is-disabled': modelValue === items}">
             <a class="pagination__link" href="#" @click.prevent="$emit('update:modelValue', modelValue + 1)">Следующая</a>
           </li>
         </ul>
@@ -50,5 +50,7 @@ export default {
 </script>
 
 <style scoped>
-
+  .form-select {
+    max-width: 30%;
+  }
 </style>
