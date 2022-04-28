d69c92b2e58cffff7dc3228f34b152d8c3c4a142
zaurmag411@gmail.com <guKGyVmC7o>
2022-04-28 22:36:36 +0300

    Feat: Add app card component

---
+++ src/components/ui/AppCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card shadow-sm card__report">
+    <header class="card-header" v-if="title">
+      <h5 class="card-header-title h6 mb-0">{{ title }}</h5>
+      <slot name="title" />
+    </header>
+
+    <div class="card-body">
+      <slot name="default" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: ['title']
+}
+</script>
+
+<style scoped>
+
+</style>
