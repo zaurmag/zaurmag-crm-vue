79eeae6cef702b30783ebbdfb73959180373bbe6
zaurmag411@gmail.com <guKGyVmC7o>
2022-05-09 11:53:09 +0300

    Upd: Add footer in card component

--- src/components/ui/AppCard.vue
+++ src/components/ui/AppCard.vue
@@ -1,20 +1,26 @@
 <template>
-  <div class="card shadow-sm card__report">
-    <header class="card-header" v-if="title">
-      <h5 class="card-header-title h6 mb-0">{{ title }}</h5>
-      <slot name="title" />
+  <div class="card shadow-sm card__report" :class="classList">
+    <slot name="prepend" />
+
+    <header class="card-header" v-if="$slots.header || title">
+      <h5 class="card-header-title h6 mb-0" v-if="title">{{ title }}</h5>
+      <slot name="header" />
     </header>
 
     <div class="card-body">
       <slot name="default" />
     </div>
+
+    <footer class="card-footer" v-if="$slots.footer">
+      <slot name="footer" />
+    </footer>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Card',
-  props: ['title']
+  props: ['title', 'classList']
 }
 </script>
 
