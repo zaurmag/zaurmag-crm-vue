d42ab07bea8ccf485f3f625984c1835dbdb57d99
zaurmag411@gmail.com <guKGyVmC7o>
2022-06-12 00:29:39 +0300

    Upd: Report graphic tooltips on dashboard

---
+++ src/use/currency-format.js
@@ -0,0 +1,9 @@
+import { currency } from '@/utils/currency'
+
+export default {
+  install (App) {
+    const currencyF = App.config.globalProperties.$currency = currency
+
+    App.provide('currency', currencyF)
+  }
+}
