73e9f74e76f9deeb20ac6563855f8a46ecae9821
zaurmag411@gmail.com <guKGyVmC7o>
2022-09-11 00:21:13 +0300

    Feat: Add new component indicator

---
+++ src/components/ui/AppIndicator.vue
@@ -0,0 +1,23 @@
+<template>
+  <span
+    :class="['indicator', classList, {[`indicator--${size}`]: size}]"
+  ></span>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  classList: {
+    type: String,
+    required: false
+  },
+  size: {
+    type: String,
+    required: false,
+    validator (value) {
+      return ['xl', 'md', 'sm'].includes(value)
+    }
+  }
+})
+</script>
