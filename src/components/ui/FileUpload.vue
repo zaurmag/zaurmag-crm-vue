c8f1e0da4cf1059efc15605a49935efdf895c7fd
zaurmag411@gmail.com <guKGyVmC7o>
2022-10-14 21:47:42 +0300

    Feat: add component FileUpload

---
+++ src/components/ui/FileUpload.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="f-upload">
+    <div class="f-upload__area">
+      <app-icon name="image" classList="f-upload__image" />
+      <h5 class="h6 f-upload__title">Перетащите файл сюда</h5>
+      <p class="f-upload__desc">или</p>
+      <span class="btn btn-primary">Загрузите</span>
+    </div>
+  </div>
+</template>
