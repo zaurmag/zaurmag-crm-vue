bd3984c00b1f3b5ccaccf77e3a3e5dfb3164d71b
zaurmag411@gmail.com <guKGyVmC7o>
2022-12-06 21:17:26 +0300

    Feat: add component AppLoaderRowPlaceholder
    Feat: add component AppLoaderRowPlaceholder

---
+++ src/components/ui/AppLoaderRowPlaceholder.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="placeholder-glow">
+    <div v-for="i in +rows" :key="i" class="row gx-2 mb-2">
+      <div
+        v-for="n in +cols"
+        :key="n"
+        class="col">
+        <div class="placeholder placeholder w-100 d-block"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  rows: {
+    type: [Number, String],
+    required: false,
+    default: 10
+  },
+  cols: {
+    type: [Number, String],
+    required: false,
+    default: 8
+  }
+})
+</script>
