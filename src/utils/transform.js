f27c4a197eb9a780d034c1cc5c440235054602ce
zaurmag411@gmail.com <guKGyVmC7o>
2022-12-15 23:31:44 +0300

    Feat: add communal settings (rates) form

--- src/utils/transform.js
+++ src/utils/transform.js
@@ -1,3 +1,11 @@
 export function transform (data) {
   return data ? Object.keys(data).map(key => ({ ...data[key], id: key })) : []
 }
+
+export function transform2 (data) {
+  return Object.keys(data).reduce((acc, key) => {
+    acc = { ...data[key], id: key }
+
+    return acc
+  }, {})
+}
