2b578de7c454a3e61909213b92ca9b70b2633f48
zaurmag411@gmail.com <guKGyVmC7o>
2022-12-22 21:42:47 +0300

    Fix: minor edits

--- src/use/user.js
+++ src/use/user.js
@@ -3,19 +3,19 @@ import { useStore } from 'vuex'
 import { useRoute } from 'vue-router'
 import breadcrumbs from '@/use/breadcrumb'
 
-export function getUser () {
-  const route = useRoute()
-  const store = useStore()
-  const user = ref(null)
+export function getUser() {
+	const route = useRoute()
+	const store = useStore()
+	const user = ref(null)
 
-  onMounted(async () => {
-    await store.dispatch('users/load')
-    user.value = await store.getters['users/userById'](route.params.id) || {}
-    const userName = route.name === 'EditProfile' ? 'редактирование: ' + user.value.name : user.value.name
-    breadcrumbs.setCurrentBreadcrumbName(userName)
-  })
+	onMounted(async () => {
+		await store.dispatch('users/load')
+		user.value = await store.getters['users/userById'](route.params.id) || {}
+		const userName = route.name === 'EditProfile' ? `редактирование: ${user.value.name}` : user.value.name
+		breadcrumbs.setCurrentBreadcrumbName(userName)
+	})
 
-  breadcrumbs.setCurrentBreadcrumbName('')
+	breadcrumbs.setCurrentBreadcrumbName('')
 
-  return user
+	return user
 }
