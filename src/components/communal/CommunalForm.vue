d49b01a454b5256467abb9c33ae32e9aee1e8c64
zaurmag411@gmail.com <guKGyVmC7o>
2022-12-24 01:47:04 +0300

    Feat: add prettier and style code

--- src/components/communal/CommunalForm.vue
+++ src/components/communal/CommunalForm.vue
@@ -1,177 +1,176 @@
 <template>
-  <form action="#" @submit.prevent="fields.onSubmit">
-    <div class="row mb-30 justify-content-between align-items-center">
-      <div class="col-lg-4 col-sm-6 order-2 order-sm-1">
-        <form-control
-          id="date"
-          type="date"
-          classListWrapper="m-0"
-          classListInput="form-control-lg"
-          v-model="fields.date"
-          :error="fields.dError"
-          @blur="fields.dBlur"
-        />
-      </div>
-      <div class="col-sm-auto order-1 order-sm-2 mb-3 mb-sm-0">
-        <form-checkbox
-          id="status"
-          :label="fields.status ? 'Оплачено' : 'Не оплачено'"
-          :classListLabel="fields.status ? 'text-primary' : 'text-secondary'"
-          v-model="fields.status"
-        />
-      </div>
-    </div>
+	<form action="#" @submit.prevent="fields.onSubmit">
+		<div class="row mb-30 justify-content-between align-items-center">
+			<div class="col-lg-4 col-sm-6 order-2 order-sm-1">
+				<form-control
+					id="date"
+					v-model="fields.date"
+					type="date"
+					class-list-wrapper="m-0"
+					class-list-input="form-control-lg"
+					:error="fields.dError"
+					@blur="fields.dBlur"
+				/>
+			</div>
+			<div class="col-sm-auto order-1 order-sm-2 mb-3 mb-sm-0">
+				<form-checkbox
+					id="status"
+					v-model="fields.status"
+					:label="fields.status ? 'Оплачено' : 'Не оплачено'"
+					:class-list-label="fields.status ? 'text-primary' : 'text-secondary'"
+				/>
+			</div>
+		</div>
 
-    <p class="h4 col mb-3">Предыдущие:</p>
-    <div class="row g-15 mb-30">
-      <form-control
-        id="addRecordElPrev"
-        label="Электричество"
-        type="number"
-        classListWrapper="col-sm"
-        classListInput="form-control-lg"
-        v-model="fields.prevElectr"
-        disabled
-      />
+		<p class="h4 col mb-3">Предыдущие:</p>
+		<div class="row g-15 mb-30">
+			<form-control
+				id="addRecordElPrev"
+				v-model="fields.prevElectr"
+				label="Электричество"
+				type="number"
+				class-list-wrapper="col-sm"
+				class-list-input="form-control-lg"
+				disabled
+			/>
 
-      <form-control
-        id="addRecordGasPrev"
-        label="Газ"
-        type="number"
-        classListWrapper="col-sm"
-        classListInput="form-control-lg"
-        v-model.number="fields.prevGas"
-        disabled
-      />
+			<form-control
+				id="addRecordGasPrev"
+				v-model.number="fields.prevGas"
+				label="Газ"
+				type="number"
+				class-list-wrapper="col-sm"
+				class-list-input="form-control-lg"
+				disabled
+			/>
 
-      <form-control
-        id="addRecordWaterPrev"
-        label="Вода"
-        type="number"
-        classListWrapper="col-sm"
-        classListInput="form-control-lg"
-        v-model.number="fields.prevWater"
-        disabled
-      />
-    </div>
+			<form-control
+				id="addRecordWaterPrev"
+				v-model.number="fields.prevWater"
+				label="Вода"
+				type="number"
+				class-list-wrapper="col-sm"
+				class-list-input="form-control-lg"
+				disabled
+			/>
+		</div>
 
-    <p class="h4 mb-3">Текущие:</p>
-    <div class="row g-15 mb-30">
-      <form-control
-        id="addRecordEl"
-        label="Электричество"
-        type="number"
-        classListWrapper="col-sm"
-        classListInput="form-control-lg"
-        v-model.number="fields.electr"
-        :error="fields.electrError"
-        @blur="fields.electrBlur"
-      />
+		<p class="h4 mb-3">Текущие:</p>
+		<div class="row g-15 mb-30">
+			<form-control
+				id="addRecordEl"
+				v-model.number="fields.electr"
+				label="Электричество"
+				type="number"
+				class-list-wrapper="col-sm"
+				class-list-input="form-control-lg"
+				:error="fields.electrError"
+				@blur="fields.electrBlur"
+			/>
 
-      <form-control
-        id="addRecordGas"
-        label="Газ"
-        type="number"
-        classListWrapper="col-sm"
-        classListInput="form-control-lg"
-        v-model.number="fields.gas"
-        :error="fields.gasError"
-        @blur="fields.gasBlur"
-      />
+			<form-control
+				id="addRecordGas"
+				v-model.number="fields.gas"
+				label="Газ"
+				type="number"
+				class-list-wrapper="col-sm"
+				class-list-input="form-control-lg"
+				:error="fields.gasError"
+				@blur="fields.gasBlur"
+			/>
 
-      <form-control
-        id="addRecordWater"
-        label="Вода"
-        type="number"
-        classListWrapper="col-sm"
-        classListInput="form-control-lg"
-        v-model.number="fields.water"
-        :error="fields.waterError"
-        @blur="fields.waterBlur"
-      />
-    </div>
+			<form-control
+				id="addRecordWater"
+				v-model.number="fields.water"
+				label="Вода"
+				type="number"
+				class-list-wrapper="col-sm"
+				class-list-input="form-control-lg"
+				:error="fields.waterError"
+				@blur="fields.waterBlur"
+			/>
+		</div>
 
-    <p class="h4 mb-3">Итог:</p>
-    <div class="row mb-30">
-      <div class="col-sm">
-        <div class="row">
-          <div class="col">
-            <span class="fz-16">{{ fields.diffElectr }} кв/ч</span>
-            <hr class="my-2">
-          </div>
-          <div class="col">
-            <span class="fz-16">{{ $currency(fields.electrAmount) }}</span>
-            <hr class="my-2">
-          </div>
-        </div>
+		<p class="h4 mb-3">Итог:</p>
+		<div class="row mb-30">
+			<div class="col-sm">
+				<div class="row">
+					<div class="col">
+						<span class="fz-16">{{ fields.diffElectr }} кв/ч</span>
+						<hr class="my-2" />
+					</div>
+					<div class="col">
+						<span class="fz-16">{{ $currency(fields.electrAmount) }}</span>
+						<hr class="my-2" />
+					</div>
+				</div>
+			</div>
+			<div class="col-sm">
+				<div class="row">
+					<div class="col">
+						<span class="fz-16">{{ fields.diffGas }} м<sup>3</sup></span>
+						<hr class="my-2" />
+					</div>
+					<div class="col">
+						<span class="fz-16">{{ $currency(fields.gasAmount) }} </span>
+						<hr class="my-2" />
+					</div>
+				</div>
+			</div>
+			<div class="col-sm">
+				<div class="row">
+					<div class="col">
+						<span class="fz-16">{{ fields.diffWater }} м<sup>3</sup></span>
+						<hr class="my-2" />
+					</div>
+					<div class="col">
+						<span class="fz-16">{{ $currency(fields.waterAmount) }}</span>
+						<hr class="my-2" />
+					</div>
+				</div>
+			</div>
+		</div>
 
-      </div>
-      <div class="col-sm">
-        <div class="row">
-          <div class="col">
-            <span class="fz-16">{{ fields.diffGas }} м<sup>3</sup></span>
-            <hr class="my-2">
-          </div>
-          <div class="col">
-            <span class="fz-16">{{ $currency(fields.gasAmount) }} </span>
-            <hr class="my-2">
-          </div>
-        </div>
-      </div>
-      <div class="col-sm">
-        <div class="row">
-          <div class="col">
-            <span class="fz-16">{{ fields.diffWater }} м<sup>3</sup></span>
-            <hr class="my-2">
-          </div>
-          <div class="col">
-            <span class="fz-16">{{ $currency(fields.waterAmount) }}</span>
-            <hr class="my-2">
-          </div>
-        </div>
-      </div>
-    </div>
+		<div class="mb-30">
+			<p class="h4 mb-3">К оплате:</p>
+			<p class="h3 text-success">{{ $currency(fields.amount) }}</p>
+		</div>
 
-    <div class="mb-30">
-      <p class="h4 mb-3">К оплате:</p>
-      <p class="h3 text-success">{{ $currency(fields.amount) }} </p>
-    </div>
+		<div class="row g-15 mb-3">
+			<div class="col-sm">
+				<p class="form-label">Прикрепить фото квитанции</p>
+				<div class="f-upload">
+					<div class="f-upload__area">
+						<app-icon name="image" class-list="f-upload__image" />
+						<h5 class="h6 f-upload__title">Перетащите файл сюда</h5>
+						<p class="f-upload__desc">или</p>
+						<span class="btn btn-primary btn-sm px-3">Загрузите</span>
+					</div>
+				</div>
+			</div>
 
-    <div class="row g-15 mb-3">
-      <div class="col-sm">
-        <p class="form-label">Прикрепить фото квитанции</p>
-        <div class="f-upload">
-          <div class="f-upload__area">
-            <app-icon name="image" classList="f-upload__image" />
-            <h5 class="h6 f-upload__title">Перетащите файл сюда</h5>
-            <p class="f-upload__desc">или</p>
-            <span class="btn btn-primary btn-sm px-3">Загрузите</span>
-          </div>
-        </div>
-      </div>
+			<form-control
+				id="addRecordDesc"
+				v-model="fields.desc"
+				label="Описание"
+				type="textarea"
+				:error="fields.descError"
+				class-list-wrapper="col-sm d-flex flex-column"
+				class-list-input="form-control-lg h-100"
+				@blur="fields.descBlur"
+			/>
+		</div>
 
-      <form-control
-        id="addRecordDesc"
-        label="Описание"
-        type="textarea"
-        :error="fields.descError"
-        @blur="fields.descBlur"
-        v-model="fields.desc"
-        classListWrapper="col-sm d-flex flex-column"
-        classListInput="form-control-lg h-100"
-      />
-    </div>
-
-    <div class="text-center">
-      <app-button
-        classListBtn="btn-primary w-100 px-4"
-        type="submit"
-        :animate="{ loading: fields.isSubmitting }"
-      >
-        Отправить
-      </app-button>
-    </div>
-  </form>
+		<div class="text-center">
+			<app-button
+				class-list-btn="btn-primary w-100 px-4"
+				type="submit"
+				:animate="{ loading: fields.isSubmitting }"
+			>
+				Отправить
+			</app-button>
+		</div>
+	</form>
 </template>
 
 <script setup>
@@ -183,10 +182,13 @@ const emit = defineEmits(['close', 'submit'])
 
 // eslint-disable-next-line no-undef
 const props = defineProps({
-  currentInitial: {
-    type: Object,
-    required: false
-  }
+	currentInitial: {
+		type: Object,
+		required: false,
+		default() {
+			return {}
+		},
+	},
 })
 
 let { ...fields } = useCommunalForm(props.currentInitial, emit)
--- src/views/CommunalView.vue
+++ src/views/CommunalView.vue
@@ -1,90 +1,93 @@
 <template>
-  <app-breadcrumb />
+	<app-breadcrumb />
 
-  <app-page title="Оплата коммунальных">
-    <template #header>
-      <div class="d-flex align-items-center">
-        <app-button
-          classListBtn="main__add-btn btn-primary shadow-sm-soft"
-          :attrs="{ 'data-bs-toggle': 'modal', 'data-bs-target': '#addCommunalRecord' }"
-          :icon="{ name: 'pencil-square', placement: 'prepend' }"
-        >
-          <span class="ms-2 d-sm-inline d-none">Добавить показания</span>
-        </app-button>
+	<app-page title="Оплата коммунальных">
+		<template #header>
+			<div class="d-flex align-items-center">
+				<app-button
+					class-list-btn="main__add-btn btn-primary shadow-sm-soft"
+					:attrs="{
+						'data-bs-toggle': 'modal',
+						'data-bs-target': '#addCommunalRecord',
+					}"
+					:icon="{ name: 'pencil-square', placement: 'prepend' }"
+				>
+					<span class="ms-2 d-sm-inline d-none">Добавить показания</span>
+				</app-button>
 
-        <app-button
-          classListBtn="btn-outline-secondary ms-10 px-3"
-          :attrs="{ 'data-bs-toggle': 'modal', 'data-bs-target': '#communalSettingForm' }"
-          :icon="{ name: 'gear', placement: 'prepend' }"
-        >
-          <span class="ms-1 d-none d-lg-inline">Тарифы</span>
-        </app-button>
-      </div>
-    </template>
+				<app-button
+					class-list-btn="btn-outline-secondary ms-10 px-3"
+					:attrs="{
+						'data-bs-toggle': 'modal',
+						'data-bs-target': '#communalSettingForm',
+					}"
+					:icon="{ name: 'gear', placement: 'prepend' }"
+				>
+					<span class="ms-1 d-none d-lg-inline">Тарифы</span>
+				</app-button>
+			</div>
+		</template>
 
-    <app-card :classList="['mb-30']">
-      <template #header>
-        <div class="row align-items-center">
-          <div class="col-xxl mb-0 mb-xl-2 mb-xxl-0 d-flex align-items-center justify-content-between">
-            <communal-list-header />
-          </div>
+		<app-card :class-list="['mb-30']">
+			<template #header>
+				<div class="row align-items-center">
+					<div
+						class="col-xxl mb-0 mb-xl-2 mb-xxl-0 d-flex align-items-center justify-content-between"
+					>
+						<communal-list-header />
+					</div>
 
-          <div class="col-xxl-auto collapse d-xl-block" id="filter">
-            <communal-filter />
-          </div>
-        </div>
-      </template>
+					<div id="filter" class="col-xxl-auto collapse d-xl-block">
+						<communal-filter />
+					</div>
+				</div>
+			</template>
 
-      <communal-list
-        :items="items"
-        :loader="loader"
-      />
+			<communal-list :items="items" :loader="loader" />
 
-    <!-- <template #footer>-->
-      <!-- <app-pagination-->
-      <!-- :count="projects.length"-->
-      <!-- :pages="PAGE_SIZE"-->
-      <!-- v-model="page"-->
-      <!-- @changeSize="changePageSize"-->
-      <!-- />-->
-    <!-- </template>-->
-    </app-card>
-  </app-page>
+			<!-- <template #footer>-->
+			<!-- <app-pagination-->
+			<!-- :count="projects.length"-->
+			<!-- :pages="PAGE_SIZE"-->
+			<!-- v-model="page"-->
+			<!-- @changeSize="changePageSize"-->
+			<!-- />-->
+			<!-- </template>-->
+		</app-card>
+	</app-page>
 
-  <teleport to="body">
-    <app-bs-modal
-      id="addCommunalRecord"
-      title="Добавить показания счетчиков"
-      :close="closeModal"
-      @hide="closeModal = false"
-      classListWrapper="modal-lg"
-						v-if="isRates"
-    >
-      <communal-form
-        @close="closeModal = true"
-      />
-    </app-bs-modal>
+	<teleport to="body">
+		<app-bs-modal
+			v-if="isRates"
+			id="addCommunalRecord"
+			title="Добавить показания счетчиков"
+			:close="closeModal"
+			class-list-wrapper="modal-lg"
+			@hide="closeModal = false"
+		>
+			<communal-form @close="closeModal = true" />
+		</app-bs-modal>
 
-    <app-bs-modal
-      id="communalSettingForm"
-      title="Тарифы"
-      :close="closeModal"
-      @hide="closeModal = false"
-    >
-      <communal-settings-form
-        v-if="isRates"
-        :initials="rates"
-        @close="closeModal = true"
-      />
-    </app-bs-modal>
+		<app-bs-modal
+			id="communalSettingForm"
+			title="Тарифы"
+			:close="closeModal"
+			@hide="closeModal = false"
+		>
+			<communal-settings-form
+				v-if="isRates"
+				:initials="rates"
+				@close="closeModal = true"
+			/>
+		</app-bs-modal>
 
-    <!-- <app-confirm-->
-    <!-- ref="confirm"-->
-    <!-- :title="'Вы удаляете ' + checkboxes.length + ' элемента'"-->
-    <!-- text="Вы уверены? Операцию нельзя будет отменить."-->
-    <!-- @resolve="removeAllConfirm"-->
-    <!-- />-->
-  </teleport>
+		<!-- <app-confirm-->
+		<!-- ref="confirm"-->
+		<!-- :title="'Вы удаляете ' + checkboxes.length + ' элемента'"-->
+		<!-- text="Вы уверены? Операцию нельзя будет отменить."-->
+		<!-- @resolve="removeAllConfirm"-->
+		<!-- />-->
+	</teleport>
 </template>
 
 <script setup>
@@ -104,9 +107,9 @@ const rates = computed(() => store.getters['communal/rates'] || {})
 const isRates = ref(null)
 
 onMounted(async () => {
-  await store.dispatch('communal/load')
-  await store.dispatch('communal/loadRates')
-  isRates.value = Object.keys(rates.value).length
-  loader.value = false
+	await store.dispatch('communal/load')
+	await store.dispatch('communal/loadRates')
+	isRates.value = Object.keys(rates.value).length
+	loader.value = false
 })
 </script>
