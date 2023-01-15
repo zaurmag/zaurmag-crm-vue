2afaa548cc2902a50e9de82d636f5b1c3e9d8931
zaurmag411@gmail.com <guKGyVmC7o>
2023-01-15 21:18:54 +0300

    Fix: ui components

--- src/components/ui/ChartLine.vue
+++ src/components/ui/ChartLine.vue
@@ -1,15 +1,15 @@
 <template>
-  <Line
-    :chart-data="chartData"
-    :chart-options="chartOptions"
-    :chartId="chartId"
-    :dataset-id-key="datasetIdKey"
-    :plugins="plugins"
-    :css-classes="cssClasses"
-    :styles="styles"
-    :width="width"
-    :height="height"
-  />
+	<Line
+		:chart-data="chartData"
+		:chart-options="chartOptions"
+		:chart-id="chartId"
+		:dataset-id-key="datasetIdKey"
+		:plugins="plugins"
+		:css-classes="cssClasses"
+		:styles="styles"
+		:width="width"
+		:height="height"
+	/>
 </template>
 
 <script>
@@ -17,55 +17,53 @@ import { Line } from 'vue-chartjs'
 import 'chart.js/auto'
 
 export default {
-  name: 'chartLine',
-  props: {
-    chartData: {
-      type: Object,
-      required: true
-    },
-    chartOptions: {
-      type: Object,
-      default: () => {
-        return {
-          responsive: true
-        }
-      }
-    },
-    chartId: {
-      type: String,
-      default: 'line-chart'
-    },
-    datasetIdKey: {
-      type: String,
-      default: 'label'
-    },
-    width: {
-      type: Number,
-      default: 400
-    },
-    height: {
-      type: Number,
-      default: 400
-    },
-    cssClasses: {
-      default: '',
-      type: String
-    },
-    styles: {
-      type: Object,
-      default: () => {}
-    },
-    plugins: {
-      type: Object,
-      default: () => {}
-    }
-  },
-  components: {
-    Line
-  }
+	name: 'ChartLine',
+	components: {
+		Line,
+	},
+	props: {
+		chartData: {
+			type: Object,
+			required: true,
+		},
+		chartOptions: {
+			type: Object,
+			default: () => {
+				return {
+					responsive: true,
+				}
+			},
+		},
+		chartId: {
+			type: String,
+			default: 'line-chart',
+		},
+		datasetIdKey: {
+			type: String,
+			default: 'label',
+		},
+		width: {
+			type: Number,
+			default: 400,
+		},
+		height: {
+			type: Number,
+			default: 400,
+		},
+		cssClasses: {
+			default: '',
+			type: String,
+		},
+		styles: {
+			type: Object,
+			default: () => {},
+		},
+		plugins: {
+			type: Object,
+			default: () => {},
+		},
+	},
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
