a31b8fca736e86e91cec519de8160c2cbcb8cc6e
zaurmag411@gmail.com <guKGyVmC7o>
2023-01-15 21:15:20 +0300

    Upd: form control toggle password

--- src/components/form/FormControl.vue
+++ src/components/form/FormControl.vue
@@ -1,125 +1,124 @@
 <template>
-  <div :class="classListWrapper">
-    <label v-if="label" :class="['form-label', classListLabel]" :for="id">{{ label }}</label>
+	<div class="form-control-wrapper">
+		<label v-if="label" :class="['form-label', classListLabel]" :for="id">{{
+			label
+		}}</label>
 
-    <slot name="prepend" />
+		<slot name="prepend" />
 
-    <div v-if="type === 'password'" class="form-control-password">
-      <input
-        :class="['form-control', { 'is-invalid': error }, classListInput]"
-        :id="id"
-        :type="type"
-        :placeholder="placeholder"
-        v-model="model"
-        @blur="$emit('blur')"
-        @input="$emit('input', $event)"
-      >
-      <button
-        class="btn btn-empty form-control-toggle p-0"
-        type="button"
-        ref="togglePass"
-      >
-        <app-icon name="eye" />
-      </button>
-    </div>
+		<div v-if="type === 'password'" class="form-control-password">
+			<input
+				:id="id"
+				v-model="model"
+				:class="['form-control', { 'is-invalid': error }, classListInput]"
+				:type="type"
+				:placeholder="placeholder"
+				@blur="$emit('blur')"
+				@input="$emit('input', $event)"
+			/>
+			<app-button
+				class-list-btn="form-control-toggle"
+				:icon="{ name: password.icon, placement: 'prepend' }"
+				@click="togglePasswordHandler"
+			/>
+		</div>
 
-    <textarea
-      v-else-if="type === 'textarea'"
-      :class="['form-control', { 'is-invalid': error }, classListInput]"
-      :id="id"
-      @blur="$emit('blur')"
-      @input="$emit('input', $event)"
-      v-model="model"
-    ></textarea>
+		<textarea
+			v-else-if="type === 'textarea'"
+			:id="id"
+			v-model="model"
+			:class="['form-control', { 'is-invalid': error }, classListInput]"
+			@blur="$emit('blur')"
+			@input="$emit('input', $event)"
+		></textarea>
 
-    <input
-      v-else
-      :class="['form-control', { 'is-invalid': error }, classListInput]"
-      :id="id"
-      :type="type"
-      :placeholder="placeholder"
-      :disabled="disabled"
-      v-model="model"
-      @blur="$emit('blur')"
-      @input="$emit('input', $event)"
-    >
-    <div class="invalid-feedback d-block fz-12" v-if="error">{{ error }}</div>
+		<input
+			v-else
+			:id="id"
+			v-model="model"
+			:class="['form-control', { 'is-invalid': error }, classListInput]"
+			:type="type"
+			:placeholder="placeholder"
+			:disabled="disabled"
+			@blur="$emit('blur')"
+			@input="$emit('input', $event)"
+		/>
+		<div v-if="error" class="invalid-feedback d-block fz-12">{{ error }}</div>
 
-    <slot name="append" />
-  </div>
+		<slot name="append" />
+	</div>
 </template>
 
-<script>
-import { ref, onMounted, computed } from 'vue'
-import togglePassword from '@/utils/toggle-password'
+<script setup>
+import { ref, computed } from 'vue'
 
-export default {
-  name: 'FormControl',
-  emits: ['blur', 'input'],
-  props: {
-    label: {
-      type: String,
-      required: false
-    },
-    id: {
-      type: String,
-      required: true
-    },
-    type: {
-      type: String,
-      required: false,
-      default: 'text'
-    },
-    modelValue: {
-      type: Object
-    },
-    error: {
-      type: String,
-      required: false
-    },
-    placeholder: {
-      type: String,
-      required: false
-    },
-    classListWrapper: {
-      type: String,
-      required: false,
-      default: 'mb-3'
-    },
-    classListInput: {
-      type: String,
-      required: false
-    },
-    classListLabel: {
-      type: String,
-      required: false
-    },
-    disabled: {
-      type: Boolean,
-      required: false
-    }
-  },
-  setup (props, { emit }) {
-    const togglePass = ref(null)
-    const model = computed({
-      get () {
-        return props.modelValue
-      },
-      set (value) {
-        emit('update:modelValue', value)
-      }
-    })
+// eslint-disable-next-line no-undef
+const emits = defineEmits('blur', 'input')
+// eslint-disable-next-line no-undef
+const props = defineProps({
+	label: {
+		type: String,
+		default: '',
+	},
+	id: {
+		type: String,
+		required: true,
+	},
+	type: {
+		type: String,
+		default: 'text',
+	},
+	modelValue: {
+		type: String,
+		default: '',
+	},
+	error: {
+		type: String,
+		default: '',
+	},
+	placeholder: {
+		type: String,
+		default: '',
+	},
+	classListInput: {
+		type: String,
+		default: '',
+	},
+	classListLabel: {
+		type: String,
+		default: '',
+	},
+	disabled: {
+		type: Boolean,
+		default: false,
+	},
+})
 
-    onMounted(() => {
-      if (togglePass.value) {
-        togglePassword(togglePass.value)
-      }
-    })
+const model = computed({
+	get() {
+		return props.modelValue
+	},
+	set(value) {
+		emits('update:modelValue', value)
+	},
+})
 
-    return {
-      togglePass,
-      model
-    }
-  }
+// Password toggle
+const togglePassRef = ref(false)
+const password = ref({
+	type: props.type,
+	icon: 'eye',
+})
+
+const togglePasswordHandler = () => {
+	togglePassRef.value = !togglePassRef.value
+
+	if (togglePassRef.value) {
+		password.value.type = 'text'
+		password.value.icon = 'eye-slash'
+	} else {
+		password.value.type = 'password'
+		password.value.icon = 'eye'
+	}
 }
 </script>
